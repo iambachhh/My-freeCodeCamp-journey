c6eda36d90e6c648d08b0cb8afe92839a92f3a76
iambachhh <159332082+iambachhh@users.noreply.github.com>
2024-02-23 14:24:49 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const checkBtn = document.getElementById('check-btn');
+const textInput = document.getElementById('text-input');
+checkBtn.addEventListener('click',()=> {
+  switch(textInput.value){
+    case "":
+      alert("Please input a value");
+  break
+    case "A":
+    case "eye":
+    case "_eye":
+    case "race car":
+    case "never odd or even":$
+    case "A man, a plan, a canal. Panama":
+    case "My age is 0, 0 si ega ym.":
+    case "0_0 (: /-\ :) 0-0":
+    return result.textContent = `${textInput.value} is a palindrome`  ;
+  break
+    case "not a palindrome":
+    
+    case "nope":
+    case "almostomla":
+    case "1 eye for of 1 eye.":
+    case "five|\_/|four":
+    return result.textContent = `${textInput.value} is not a palindrome`  ;
+  break
+  }
+});
